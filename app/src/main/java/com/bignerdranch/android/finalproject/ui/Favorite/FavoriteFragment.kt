fd439f52122ea69fd2d9c9a29073ede673df197f
Robin <cy615808@gmail.com>
2021-10-06 23:14:55 -0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/finalproject/ui/Favorite/FavoriteFragment.kt
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.finalproject.ui.Favorite
+
+import androidx.fragment.app.Fragment
+
+class FavoriteFragment : Fragment()  {
+}
---
+++ app/src/main/java/com/bignerdranch/android/finalproject/ui/OwnRecipe/OwnRecipeFragment.kt
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.finalproject.ui.OwnRecipe
+
+import androidx.fragment.app.Fragment
+
+class OwnRecipeFragment : Fragment()   {
+}
